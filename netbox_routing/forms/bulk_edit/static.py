939065bfef8c5e65753f04cdc6081b37f912262e
Daniel Sheppard <dans@dansheps.com>
2024-09-16 15:15:51 -0500

    Update bulk_edit forms

---
+++ netbox_routing/forms/bulk_edit/static.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.utils.translation import gettext as _
+
+from dcim.models import Device
+from ipam.models import VRF
+from netbox.forms import NetBoxModelBulkEditForm
+from utilities.forms import BOOLEAN_WITH_BLANK_CHOICES
+from utilities.forms.fields import DynamicModelChoiceField, DynamicModelMultipleChoiceField
+from utilities.forms.rendering import FieldSet
+
+from netbox_routing.models import StaticRoute
+
+
+__all__ = (
+    'StaticRouteBulkEditForm',
+)
+
+
+class StaticRouteBulkEditForm(NetBoxModelBulkEditForm):
+    devices = DynamicModelMultipleChoiceField(
+        label='Device',
+        queryset=Device.objects.all(),
+        required=False,
+        selector=True,
+    )
+    vrf = DynamicModelChoiceField(
+        label='VRF',
+        queryset=VRF.objects.all(),
+        required=False,
+        selector=True,
+    )
+    metric = forms.IntegerField(label=_('Metric'), required=False)
+    permanent = forms.ChoiceField(label=_('Permanent'), choices=BOOLEAN_WITH_BLANK_CHOICES, required=False)
+
+    model = StaticRoute
+    fieldsets = (
+        FieldSet('devices', 'vrf', 'prefix', 'next_hop', name='Route'),
+        FieldSet('metric', 'permanent', name='Attributes'),
+        FieldSet('description', 'comments')
+    )
+    nullable_fields = ('devices', 'vrf', 'metric', 'permanent', 'description', 'comments')
