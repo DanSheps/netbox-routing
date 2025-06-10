c8cf6fb0293f727784aed865dd1a1b2427d52f61
Matej Hyks <matej.hyks@seznam.cz>
2025-06-10 11:55:01 +0200

    02-ospf remove colored button to reflect netbox changes

--- netbox_routing/navigation/ospf.py
+++ netbox_routing/navigation/ospf.py
@@ -12,12 +12,11 @@ ospf_instance = PluginMenuItem(
     link_text='Instances',
     permissions=['netbox_routing.view_ospfinstance'],
     buttons=(
-        PluginMenuButton('plugins:netbox_routing:ospfinstance_add', 'Add', 'mdi mdi-plus', ButtonColorChoices.GREEN),
+        PluginMenuButton('plugins:netbox_routing:ospfinstance_add', 'Add', 'mdi mdi-plus',),
         PluginMenuButton(
             'plugins:netbox_routing:ospfinstance_import',
             'Import',
             'mdi mdi-upload',
-            ButtonColorChoices.CYAN
         ),
     )
 )
@@ -26,12 +25,11 @@ ospf_area = PluginMenuItem(
     link_text='Areas',
     permissions=['netbox_routing.view_ospfarea'],
     buttons=(
-        PluginMenuButton('plugins:netbox_routing:ospfarea_add', 'Add', 'mdi mdi-plus', ButtonColorChoices.GREEN),
+        PluginMenuButton('plugins:netbox_routing:ospfarea_add', 'Add', 'mdi mdi-plus', ),
         PluginMenuButton(
             'plugins:netbox_routing:ospfarea_import',
             'Import',
             'mdi mdi-upload',
-            ButtonColorChoices.CYAN
         ),
     )
 )
@@ -40,12 +38,11 @@ ospf_interfaces = PluginMenuItem(
     link_text='Interfaces',
     permissions=['netbox_routing.view_ospfinterface'],
     buttons=(
-        PluginMenuButton('plugins:netbox_routing:ospfinterface_add', 'Add', 'mdi mdi-plus', ButtonColorChoices.GREEN),
+        PluginMenuButton('plugins:netbox_routing:ospfinterface_add', 'Add', 'mdi mdi-plus',),
         PluginMenuButton(
             'plugins:netbox_routing:ospfinterface_import',
             'Import',
             'mdi mdi-upload',
-            ButtonColorChoices.CYAN
         ),
     )
 )
