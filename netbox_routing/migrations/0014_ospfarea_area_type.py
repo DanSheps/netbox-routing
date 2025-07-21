30d9da76825a70bcd74ed7b3039cdeab9d155a57
Daniel Sheppard <dans@dansheps.com>
2025-07-20 21:38:57 -0500

    Fix migration

--- netbox_routing/migrations/0014_ospfarea_area_type.py
+++ netbox_routing/migrations/0014_ospfarea_area_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("netbox_routing", "0012_osfp_instance_vrf"),
+        ("netbox_routing", "0013_staticroute_tag"),
     ]
 
     operations = [
