338f94685ee0c86281bca277f972a0be1507bdc9
Matej Hyks <matej.hyks@seznam.cz>
2025-08-18 11:36:56 +0200

    formating

--- netbox_routing/models/eigrp.py
+++ netbox_routing/models/eigrp.py
@@ -38,7 +38,7 @@ class EIGRPRouter(PrimaryModel):
 
     class Meta:
         verbose_name = 'EIGRP Router'
-        unique_together=[['device', 'name']]
+        unique_together = [['device', 'name']]
 
     def __str__(self):
         if self.pid:
