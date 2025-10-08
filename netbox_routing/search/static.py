a8829ca3509faf246466c07d56dafdf3e5a915a9
lumarel <lukas@magauer.eu>
2025-10-08 22:24:20 +0200

    Add search indexes

---
+++ netbox_routing/search/static.py
@@ -0,0 +1,12 @@
+from netbox.search import SearchIndex, register_search
+from netbox_routing.models.static import StaticRoute
+
+@register_search
+class StaticRouteIndex(SearchIndex):
+    model = StaticRoute
+    fields = (
+        ('name', 100),
+        ('prefix', 200),
+        ('next_hop', 300),
+        ('comments', 5000),
+    )
