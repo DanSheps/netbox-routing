8b0ba21e3a4e570d19574b5c963989e44183802a
Daniel Sheppard <dans@dansheps.com>
2026-01-29 12:40:58 -0600

    Add Searching for BGP

---
+++ netbox_routing/search/__init__.py
@@ -0,0 +1,23 @@
+from .bgp import *
+from .community import *
+from .objects import *
+
+__all__ = (
+    'ASPathIndex',
+    'ASPathEntryIndex',
+    'BGPRouterIndex',
+    'BGPScopeIndex',
+    'BGPAddressFamilyIndex',
+    'BGPPeerIndex',
+    'BGPPeerAddressFamilyIndex',
+    'BGPPeerTemplateIndex',
+    'BGPPolicyTemplateIndex',
+    'BGPSessionTemplateIndex',
+    'CommunityIndex',
+    'CommunityListIndex',
+    'CommunityListEntryIndex',
+    'PrefixListIndex',
+    'PrefixListEntryIndex',
+    'RouteMapIndex',
+    'RouteMapEntryIndex',
+)
