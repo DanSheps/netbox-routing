66f7b2120da483714300c0a693420d70b2b7ae11
Arturo <baldoarturo@gmail.com>
2026-02-18 12:55:59 -0300

    Fix bulk imports

--- netbox_routing/forms/bulk_import/ospf.py
+++ netbox_routing/forms/bulk_import/ospf.py
@@ -3,9 +3,11 @@ from django.utils.translation import gettext as _
 from dcim.models import Interface, Device
 from ipam.models import VRF
 from netbox.forms import NetBoxModelImportForm
-from utilities.forms.fields import CSVModelChoiceField
+from utilities.forms.fields import CSVModelChoiceField, CSVChoiceField
+from netbox_routing.choices.ospf import OSPFNetworkTypeChoices
 
 from netbox_routing.models import OSPFInstance, OSPFArea, OSPFInterface
+
 
 __all__ = (
     "OSPFInstanceImportForm",
@@ -80,6 +82,11 @@ class OSPFInterfaceImportForm(NetBoxModelImportForm):
         to_field_name="name",
         help_text=_("Name of interface"),
     )
+    network_type = CSVChoiceField(
+        choices=OSPFNetworkTypeChoices.CHOICES,
+        required=True,
+        help_text=_("OSPF network type"),
+    )
 
     class Meta:
         model = OSPFInterface
@@ -88,6 +95,7 @@ class OSPFInterfaceImportForm(NetBoxModelImportForm):
             "instance",
             "area",
             "interface",
+            "network_type",
             "passive",
             "priority",
             "bfd",
