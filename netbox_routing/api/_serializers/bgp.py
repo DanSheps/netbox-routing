e6958b36d666e54a4c8950fd90635732ceb7a29e
Matěj Hykš <matej.hyks@egd.cz>
2026-08-24 07:09:42 +0200

    change API serializers

--- netbox_routing/api/_serializers/bgp.py
+++ netbox_routing/api/_serializers/bgp.py
@@ -173,10 +173,10 @@ class BGPRouterSerializer(NetBoxModelSerializer):
     asn = ASNSerializer(nested=True)
     settings = BGPSettingSerializer(many=True, required=False)
     tenant = TenantSerializer(nested=True, required=False)
-    peer_templates = BGPSessionTemplateSerializer(
+    peer_templates = BGPPeerTemplateSerializer(
         many=True, nested=True, required=False
     )
-    policy_templates = BGPSessionTemplateSerializer(
+    policy_templates = BGPPolicyTemplateSerializer(
         many=True, nested=True, required=False
     )
     session_templates = BGPSessionTemplateSerializer(
